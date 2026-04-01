469ae854c3c26c3a0b3b0eaacf5c9afd90b5db82
Pedram Badakhchani <pb357@cam.ac.uk>
2026-04-01 12:44:59 +0100

    adjust section spacing when info callout is present

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -192,7 +192,7 @@ export class Section extends React.PureComponent {
 									/>
 								) : null}
 								<div
-									className={`pb-5 text-sm text-slate-500`}
+									className={`pb-5 text-sm text-slate-500 ${(informationText || informationTextHTML) ? "mt-2" : ""}`}
 								>
 									{enhancedChildren}
 								</div>
